5463cf024a5dd41dee231d160e2683b02932a328
Michaël Veillet Guillem <micou.veillet@gmail.com>
2018-10-22 17:29:40 +0200

    correction bug ORM and added the posibilities to update Zoho

--- ORM.php
+++ ORM.php
@@ -48,13 +48,15 @@ class ORM {
 
     public function __construct()
     {
-        $this->db = $Database()->getConnection();
-//        $this->zoho = jsepfza;
+        $database = new Database();
+        $this->db = $database->getConnection();
+        $this->zoho = new Zoho($this->db);
     }
 
     public function __destruct()
     {
-        //
+        $this->db = null;
+        $this->zoho = null;
     }
 
     public function __unset()
@@ -79,10 +81,10 @@ class ORM {
         $stmt = $this->db->prepare('SELECT * FROM HELPER WHERE id_h=:id_h');
         $stmt->bndParam(':id_h', $id);
         if ($stmt->rowCount() == 1)
-            $result = new Helper($stmt->fetch();
+            $result = new Helper($stmt->fetch());
         else if ($stmt->rowCount() > 1) {
             printLog(__METHOD__, 'Helper id not unique. /!\\', true);
-            $result = new Helper($stmt->fetch();
+            $result = new Helper($stmt->fetch());
         } else
             printLog(__METHOD__, 'Helper id: '.$id.' not found');
         printLog(__METHOD__, 'Helper id '.$id'. found ');
@@ -97,10 +99,10 @@ class ORM {
         $stmt = $this->db->prepare('SELECT * FROM CLIENT WHERE id_c=:id_c');
         $stmt->bndParam(':id_c', $id);
         if ($stmt->rowCount() == 1)
-            $result = new Client($stmt->fetch();
+            $result = new Client($stmt->fetch());
         else if ($stmt->rowCount() > 1) {
             printLog(__METHOD__, 'Client id not unique. /!\\', true);
-            $result = new Client($stmt->fetch();
+            $result = new Client($stmt->fetch());
         } else
             printLog(__METHOD__, 'Client id: '.$id.' not found');
         printLog(__METHOD__, 'Client id '.$id'. found ');
@@ -120,10 +122,10 @@ class ORM {
         $stmt = $this->db->prepare('SELECT * FROM MISSION WHERE id_m=:id_m');
         $stmt->bndParam(':id_m', $id);
         if ($stmt->rowCount() == 1)
-            $result = new Mission($stmt->fetch();
+            $result = new Mission($stmt->fetch());
         else if ($stmt->rowCount() > 1) {
             printLog(__METHOD__, 'Mission id not unique. /!\\', true);
-            $result = new Mission($stmt->fetch();
+            $result = new Mission($stmt->fetch());
         } else
             printLog(__METHOD__, 'Mission id: '.$id.' not found');
         printLog(__METHOD__, 'Mission id '.$id'. found ');
@@ -142,10 +144,10 @@ class ORM {
         $stmt = $this->db->prepare('SELECT * FROM MISSION WHERE ref_m=:ref_m');
         $stmt->bndParam(':ref_m', $ref);
         if ($stmt->rowCount() == 1)
-            $result = new Mission($stmt->fetch();
+            $result = new Mission($stmt->fetch());
         else if ($stmt->rowCount() > 1) {
             printLog(__METHOD__, 'Mission ref not unique. /!\\', true);
-            $result = new Mission($stmt->fetch();
+            $result = new Mission($stmt->fetch());
         } else
             printLog(__METHOD__, 'Mission ref: '.$ref.' not found');
         printLog(__METHOD__, 'Mission ref '.$ref'. found ');
@@ -160,10 +162,10 @@ class ORM {
         $stmt = $this->db->prepare('SELECT * FROM PRESTATION WHERE id_presta=:id_presta');
         $stmt->bndParam(':id_presta', $id);
         if ($stmt->rowCount() == 1)
-            $result = new Prestation($stmt->fetch();
+            $result = new Prestation($stmt->fetch());
         else if ($stmt->rowCount() > 1) {
             printLog(__METHOD__, 'Prestation id not unique. /!\\', true);
-            $result = new Prestation($stmt->fetch();
+            $result = new Prestation($stmt->fetch());
         } else
             printLog(__METHOD__, 'Prestation id: '.$id.' not found');
         printLog(__METHOD__, 'Prestation id '.$id'. found ');
